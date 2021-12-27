62c51e9eb9027115361aa9521733cedb5f24790d
XGboost2 <kuralarasu.venkatesh@gmail.com>
2021-12-27 14:55:35 +0100

    creating components
    Creating components for taskify

---
+++ src/components/Button.jsx
@@ -0,0 +1,21 @@
+import PropTypes from 'prop-types'
+
+const Button = ({color,text,onClick}) => {
+    return (
+        <button 
+        className='btn' 
+        onClick={onClick}
+        style={{backgroundColor:color}}>{text}</button>
+    )
+}
+
+Button.defaultProps={
+    color:'steelblue'
+}
+
+Button.propTypes={
+    'text': PropTypes.string.isRequired
+}
+
+export default Button
+
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,16 @@
+import Button from './Button'
+
 const Header = () => {
+
+    const onClick = () => {
+        console.log('click')
+    }
+
     return (
         <div>
-            <header>
+            <header className='header'>
             <h1>Trakify</h1>
+            <Button color='green' text='Add Task' onClick={onClick}/>
             </header>
         </div>
     )
